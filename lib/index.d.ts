bfc4b660e9d963122ee1e794f6099b26129365ee
joon-prnd <joon@prnd.co.kr>
2022-06-10 15:25:41 +0900

    chore: add index.d.ts

---
+++ lib/index.d.ts
@@ -0,0 +1,16 @@
+// Fork from @types/body-scroll-lock
+// Type definitions for body-scroll-lock 3.1
+// Project: https://github.com/willmcpo/body-scroll-lock
+// Definitions by: kimcoder <https://github.com/kimcoder>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export interface BodyScrollOptions {
+  reserveScrollBarGap?: boolean | undefined;
+  allowTouchMove?: ((el: HTMLElement | Element) => boolean) | undefined;
+}
+
+export function disableBodyScroll(targetElement: HTMLElement | Element, options?: BodyScrollOptions): void;
+export function enableBodyScroll(targetElement: HTMLElement | Element): void;
+export function clearAllBodyScrollLocks(): void;
+
+export as namespace bodyScrollLock;
